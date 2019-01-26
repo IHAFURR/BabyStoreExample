89f955e5b4e3f522c465caeb18025fafe1aaf5fc
IHAFURR <chernenkoyuriy1992@gmail.com>
2019-01-26 17:31:26 +0200

    implement Authentication option to Individual User Accounts

---
+++ BabyStore/Migrations/201901261528091_AddIdentityModel.cs
@@ -0,0 +1,16 @@
+namespace BabyStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIdentityModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
