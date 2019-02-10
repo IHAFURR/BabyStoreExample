20949ee69f7c733fc3964b6156173c0b5d4f7fd5
IHAFURR <chernenkoyuriy1992@gmail.com>
2019-02-10 17:27:35 +0200

    Add User controller and index view

---
+++ BabyStore/Controllers/UserAdminController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BabyStore.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UserAdminController : Controller
+    {
+
+        private ApplicationUserManager _applicationUserManager;
+        private ApplicationRoleManager _applicationRoleManager;
+        public UserAdminController()
+        {
+        }
+
+        public UserAdminController(ApplicationUserManager userManager, ApplicationRoleManager roleManager)
+        {
+            ApplicationUserManager = userManager;
+            ApplicationRoleManager = roleManager;
+        }
+
+        public ApplicationRoleManager ApplicationRoleManager {
+            get
+            {
+                return _applicationRoleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            set
+            {
+                _applicationRoleManager = value;
+            }
+        }
+
+        public ApplicationUserManager ApplicationUserManager {
+            get
+            {
+                return _applicationUserManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            set
+            {
+                _applicationUserManager = value;
+            }
+        }
+
+
+        // GET: UserAdmin
+        public async Task<ActionResult> Index()
+        {
+            return View(await ApplicationUserManager.Users.ToListAsync());
+        }
+
+        // GET: UserAdmin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserAdmin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserAdmin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserAdmin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserAdmin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserAdmin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
