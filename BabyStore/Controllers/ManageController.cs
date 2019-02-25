5f34fe0016cbe6a0e67eac33fb13e2e4af60b05c
IHAFURR <chernenkoyuriy1992@gmail.com>
2019-02-25 18:45:13 +0200

    Allow user edit and see details

--- BabyStore/Controllers/ManageController.cs
+++ BabyStore/Controllers/ManageController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using BabyStore.Models;
+using BabyStore.ViewModel;
 
 namespace BabyStore.Controllers
 {
@@ -52,27 +53,50 @@ namespace BabyStore.Controllers
 
         //
         // GET: /Manage/Index
-        public async Task<ActionResult> Index(ManageMessageId? message)
+        public async Task<ActionResult> Index()
         {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
-                : message == ManageMessageId.SetPasswordSuccess ? "Your password has been set."
-                : message == ManageMessageId.SetTwoFactorSuccess ? "Your two-factor authentication provider has been set."
-                : message == ManageMessageId.Error ? "An error has occurred."
-                : message == ManageMessageId.AddPhoneSuccess ? "Your phone number was added."
-                : message == ManageMessageId.RemovePhoneSuccess ? "Your phone number was removed."
-                : "";
-
             var userId = User.Identity.GetUserId();
-            var model = new IndexViewModel
+            var user = await UserManager.FindByIdAsync(userId);
+            return View(user);
+        }
+
+
+        // GET: /Manage/Edit
+        public async Task<ActionResult> Edit()
+        {            
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            var model = new EditUserViewModel
             {
-                HasPassword = HasPassword(),
-                PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
-                TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
-                Logins = await UserManager.GetLoginsAsync(userId),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
+                Email = user.Email,
+                DateOfBirth = user.DateOfBirth,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Address = user.Address
             };
             return View(model);
+        }
+
+
+        // POST: /Manage/Edit
+        // To protect from overposting attacks, please enable the specific properties you want 
+        // to bind to, for more details visit http://go.microsoft.com/fwlink/?LinkId=317598
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditPost()
+        {
+            var userId = User.Identity.GetUserId();
+            var userToUpdate = await UserManager.FindByIdAsync(userId);
+            if (TryUpdateModel(userToUpdate, "", new string[] {
+                "FirstName",
+                "LastName",
+                "DateOfBirth",
+                "Address"
+            }))
+            {
+                await UserManager.UpdateAsync(userToUpdate);
+                return RedirectToAction("Index");
+            }
+            return View();
         }
 
         //
